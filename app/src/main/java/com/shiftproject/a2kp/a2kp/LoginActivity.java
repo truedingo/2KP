40829d5c64fc8f159b1f89a0887ec34eafcd5d77
Diogo Amores <dingo@Diogos-MacBook-Pro-3.local>
2018-04-20 18:54:47 +0100

    first version, nothing implemented just android studio project with firebase

---
+++ app/src/main/java/com/shiftproject/a2kp/a2kp/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.shiftproject.a2kp.a2kp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
