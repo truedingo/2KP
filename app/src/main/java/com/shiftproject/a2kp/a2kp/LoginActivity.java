48bd51157265e3461edb4ad01ff396939daa67aa
Diogo Amores <dingo@Diogos-MacBook-Pro-3.local>
2018-04-20 22:13:05 +0100

    login com face a funcionar

--- app/src/main/java/com/shiftproject/a2kp/a2kp/LoginActivity.java
+++ app/src/main/java/com/shiftproject/a2kp/a2kp/LoginActivity.java
@@ -1,75 +1,74 @@
 package com.shiftproject.a2kp.a2kp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.facebook.CallbackManager;
-import com.google.firebase.FirebaseApp;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import com.facebook.AccessToken;
 public class LoginActivity extends AppCompatActivity {
-    private FirebaseAuth mAuth;
-    private CallbackManager callbackManager;
-    private AccessToken facebookAcessToken;
-    private FirebaseUser currentUser;
-    private FirebaseAuth firebaseAuth;
-
-
+    public static final String TAG = "LoginActivity";
+    public CallbackManager callbackManager;
+    public AccessToken facebookAcessToken;
+    public FirebaseUser currentUser;
+    public FirebaseAuth firebaseAuth;
+    public LoginButton loginBtn;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        firebaseAuth = FirebaseAuth.getInstance();
+        FacebookSdk.setApplicationId("162080441155693");
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_login);
-        mAuth = FirebaseAuth.getInstance();
+        initializeFacebook();
 
     }
 
-    protected  void onStart(){
+    protected void onStart() {
         super.onStart();
         currentUser = firebaseAuth.getCurrentUser();
-        if(currentUser != null){
+        if (currentUser != null) {
             //change to new activity
         }
 
     }
 
-    private void initializeFacebook(){
+    private void initializeFacebook() {
         // Initialize Facebook Login button
         callbackManager = CallbackManager.Factory.create();
-        //LoginButton loginButton = findViewById(R.id.button_facebook_login);
-        //loginButton.setReadPermissions("email", "public_profile");
-        //loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+        loginBtn = findViewById(R.id.loginButton);
+        loginBtn.setReadPermissions("email", "public_profile");
+        loginBtn.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 Log.d(TAG, "facebook:onSuccess:" + loginResult);
-                handleFacebookAccessToken(loginResult.getAccessToken());
+                facebookAcessToken = loginResult.getAccessToken();
             }
-
             @Override
             public void onCancel() {
                 Log.d(TAG, "facebook:onCancel");
                 // ...
             }
-
             @Override
             public void onError(FacebookException error) {
                 Log.d(TAG, "facebook:onError", error);
-                // ...
             }
         });
-
-
-        @Override
-        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-            super.onActivityResult(requestCode, resultCode, data);
-
-            // Pass the activity result back to the Facebook SDK
-            CallbackManager.onActivityResult(requestCode, resultCode, data);
-        }
-
     }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 }
