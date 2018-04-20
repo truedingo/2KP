5d20b54fdf96e5a7c1a67b4616233c606299f5af
Diogo Amores <dingo@Diogos-MacBook-Pro-3.local>
2018-04-20 19:14:54 +0100

    firebase

--- app/src/main/java/com/shiftproject/a2kp/a2kp/LoginActivity.java
+++ app/src/main/java/com/shiftproject/a2kp/a2kp/LoginActivity.java
@@ -3,11 +3,17 @@ package com.shiftproject.a2kp.a2kp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_login);
     }
 }
